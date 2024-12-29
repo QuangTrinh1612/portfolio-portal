55d5f9ef5405efa112d03822bd6d67422dd3ee70
QuangTrinh1612 <trinhquocquang.buh@gmail.com>
2024-12-29 12:43:13 +0000

    Update JS

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'lewis', // Usually your GitHub org/user name.
-  projectName: 'lewisblog.github.io', // Usually your repo name.
+  projectName: 'QuangTrinh1612.github.io', // Usually your repo name.
   trailingSlash: false,
   deploymentBranch: 'main',
 
